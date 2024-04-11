268d4e0162c728185e1841085e6ad9b9eaa3a3fd
Anna <aksmithnw@gmail.com>
2024-04-11 13:54:38 -0700

    Add skeleton code for GameController
    Create GameController factory function that returns only the functions
    necessary to play from the console: playRound, getActivePlayer,
    getBoard, and getWinner

---
+++ gameScript.js
@@ -0,0 +1,29 @@
+function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+
+    const playRound = (i, j) => {
+        // add mark to board
+        // check if win
+        // switch player turn
+    }
+
+    const getActivePlayer = () => {
+        // return active player
+    }
+
+    const getBoard = () => {
+        // return board
+    }
+
+    const getWinner = () => {
+        // return winner
+    }
+
+    return {
+        playRound,
+        getActivePlayer,
+        getBoard,
+        getWinner,
+    };
+}
+
+
