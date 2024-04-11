11748345f2a810916f9ed9b1c8e41d6ad78d8050
Anna <aksmithnw@gmail.com>
2024-04-11 16:49:45 -0700

    Implement win logic

--- gameScript.js
+++ gameScript.js
@@ -19,13 +19,17 @@ function Cell() {
     };
   }
 
-function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+const gameController = (function(playerOneName = "Player One", playerTwoName = "Player Two") {
     const players = [Player(playerOneName, "X"), Player(playerTwoName, "O")];
     let activePlayerIdx = 0;
     let winningPlayerIdx = -1;
 
     const switchActivePlayer = () => {
         activePlayerIdx = (activePlayerIdx + 1) % 2;
+    }
+
+    const setWinningPlayer = () => {
+        winningPlayerIdx = activePlayerIdx;
     }
 
     const gameBoard = (function () {
@@ -53,10 +57,36 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
             console.log(boardWithCellValues);
         }
 
+        const hasCompleteLine = (mark) => {
+            // check rows
+            for (let i = 0; i < BOARD_SIZE; i++) {
+                let isPerfectRow = true;
+                for (let j=0; j < BOARD_SIZE; j++) isPerfectRow &= (board[i][j].getValue() === mark);
+                if (isPerfectRow) return true;
+            }
+    
+            // check columns 
+            for (let j = 0; j < BOARD_SIZE; j++) {
+                let isPerfectCol = true;
+                for (let i=0; i < BOARD_SIZE; i++) isPerfectCol &= (board[i][j].getValue() === mark);
+                if (isPerfectCol) return true;
+            }
+
+            // check diagonals
+            let isPerfectDiagonal = true;
+            for (let i = 0; i < BOARD_SIZE; i++) isPerfectDiagonal &= (board[i][i].getValue() === mark);
+            if (isPerfectDiagonal) return true;
+
+            isPerfectDiagonal = true;
+            for (let i = 0; i < BOARD_SIZE; i++) isPerfectDiagonal &= (board[i][BOARD_SIZE-i-1].getValue() === mark);
+            return isPerfectDiagonal;
+        }
+
         return {
             addMark,
             getBoard,
-            printBoard
+            printBoard,
+            hasCompleteLine,
         };
     })();
 
@@ -65,10 +95,15 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
         gameBoard.addMark(i, j, getActivePlayer().mark);
 
         // check if win
-        // switch player turn
-        switchActivePlayer();
+        gameBoard.hasCompleteLine(getActivePlayer().mark) ? setWinningPlayer() : switchActivePlayer();
 
         gameBoard.printBoard();
+        
+        if (isGameOver()) {
+            console.log(`${getWinner().name} wins!`);
+        } else {
+            console.log(`${getActivePlayer().name}'s turn`);
+        }
     }
 
     const getActivePlayer = () => {
@@ -78,15 +113,50 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
 
     const getWinner = () => {
         // return winner
-        return winningPlayerIdx = -1 ? null : players[winnerIdx];
+        return winningPlayerIdx === -1 ? null : players[winningPlayerIdx];
     }
+
+    const isGameOver = () => {
+        return winningPlayerIdx !== -1;
+    }
+
+    gameBoard.printBoard();
+    console.log(`${getActivePlayer().name}'s turn`);
 
     return {
         playRound,
         getActivePlayer,
         getBoard : () => gameBoard.getBoard(),
+        isGameOver,
         getWinner,
     };
-}
+})();
+
+// column
+/*
+gameController.playRound(0,0);
+gameController.playRound(2,2);
+gameController.playRound(1,0);
+gameController.playRound(2,2);
+gameController.playRound(2,0);
+*/
+
+// diagonal 1
+/*
+gameController.playRound(0,0);
+gameController.playRound(1,0);
+gameController.playRound(1,1);
+gameController.playRound(1,0);
+gameController.playRound(2,2);
+*/
+
+// diagonal 2
+/*
+gameController.playRound(0,2);
+gameController.playRound(1,0);
+gameController.playRound(1,1);
+gameController.playRound(1,0);
+gameController.playRound(2,0);
+*/
 
 
