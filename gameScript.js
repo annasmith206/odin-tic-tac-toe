269fdc40e4133c7aab8224de44ca8a3f0fc8162f
Anna <aksmithnw@gmail.com>
2024-04-11 14:03:48 -0700

    Add player related code
    Create player factory function with name and mark (X or O), then in
    GameController, initialize array of two players and keep track of
    active/winning players by storing indicies. Implement
    functions to retrieve active/winning player, and update active player

--- gameScript.js
+++ gameScript.js
@@ -1,13 +1,26 @@
+function Player(name, mark) {
+    return {name, mark};
+}
+
 function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+    const players = [Player(playerOneName, "X"), Player(playerTwoName, "O")];
+    const activePlayerIdx = 0;
+    const winningPlayerIdx = -1;
+
+    const switchActivePlayer = () => {
+        activePlayerIdx = (activePlayerIdx + 1) % 2;
+    }
 
     const playRound = (i, j) => {
         // add mark to board
         // check if win
         // switch player turn
+        switchActivePlayer();
     }
 
     const getActivePlayer = () => {
         // return active player
+        return players[activePlayerIdx];
     }
 
     const getBoard = () => {
@@ -16,6 +29,7 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
 
     const getWinner = () => {
         // return winner
+        return winningPlayerIdx = -1 ? null : players[winnerIdx];
     }
 
     return {
